391e24364e6cd0d36160d820d332e5270a714c8d
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-01-17 20:47:08 -0300

    Implemetação da NavBar, e do Router inicial

---
+++ src/pages/inicial/Inicial.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import bikcraft from "../../assets/img/bikcraft.svg";
+import "./inicial.css";
+
+function Inicial() {
+  return (
+    <header className="header-bg">
+      <div className="header">
+        <img src={bikcraft} alt="Bikcraft" />
+        <nav aria-label="primaria">
+          <ul className="header-menu">
+            <li>
+              <Link to="/bicicletas">Bicicletas</Link>
+            </li>
+            <li>
+              <Link to="/seguros">Seguros</Link>
+            </li>
+            <li>
+              <Link to="/contato">Contatos</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Inicial;
