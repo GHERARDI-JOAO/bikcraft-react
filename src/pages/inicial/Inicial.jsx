1fcec4888cdbdffe72baecd8bb28f954af072c47
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-01-19 21:39:24 -0300

    Adicionada parte das bicicletas na página inicial, também feitos ajustes utilitários.

--- src/pages/inicial/Inicial.jsx
+++ src/pages/inicial/Inicial.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import introducao from "../../assets/img/fotos/introducao.jpg";
 import NavBar from "../../components/NavBar/NavBar";
 import "./inicial.css";
+import Magic from "../../assets/img/bicicletas/magic-home.jpg";
+import Nimbus from "../../assets/img/bicicletas/nimbus-home.jpg";
+import Nebula from "../../assets/img/bicicletas/nebula-home.jpg";
 
 function Inicial() {
   return (
@@ -27,6 +30,36 @@ function Inicial() {
           </div>
         </div>
       </main>
+
+      <article className="bicicletas-lista">
+        <h2 className="font-1-xxl">
+          escolha a sua <span className="cor-p1">.</span>
+        </h2>
+
+        <ul>
+          <li>
+            <a href="./bicicletas/magic">
+              <img src={Magic} alt="Bicicleta preta" />
+              <h3 className="font-1-xl">Magic Might</h3>
+              <span className="font-2-m cor-8">R$ 2499</span>
+            </a>
+          </li>
+          <li>
+            <a href="./bicicletas/magic">
+              <img src={Nimbus} alt="Bicicleta preta" />
+              <h3 className="font-1-xl">Nimbus Stark</h3>
+              <span className="font-2-m cor-8">R$ 4999</span>
+            </a>
+          </li>
+          <li>
+            <a href="./bicicletas/magic">
+              <img src={Nebula} alt="Bicicleta preta" />
+              <h3 className="font-1-xl">Nebula Cosmic</h3>
+              <span className="font-2-m cor-8">R$ 3999</span>
+            </a>
+          </li>
+        </ul>
+      </article>
     </>
   );
 }
