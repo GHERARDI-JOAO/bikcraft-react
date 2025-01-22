12389bfa330a5ccc1e18c0e2c44d4b760fb4fba4
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-01-22 15:16:41 -0300

    Nova página Termos criada e adicionada ao router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicial from "./pages/inicial/Inicial";
+import Termos from "./pages/Termos/Termos";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         {/* Página Inicial */}
         <Route path="/" element={<Inicial />} />
         {/* Outras páginas */}
+        <Route path="/termos" element={<Termos />} />
       </Routes>
     </BrowserRouter>
   );
